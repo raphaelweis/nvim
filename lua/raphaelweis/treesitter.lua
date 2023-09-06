d50050295a28ad5aa0ea1c1e4672203d110b2882
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-09-06 13:12:44 +0200

    changes

--- lua/raphaelweis/treesitter.lua
+++ lua/raphaelweis/treesitter.lua
@@ -1,6 +1,21 @@
 require('nvim-treesitter.configs').setup {
 	-- Add languages to be installed here that you want installed for treesitter
-	ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'dart' },
+	ensure_installed = {
+		'c',
+		'cpp',
+		'go',
+		'lua',
+		'python',
+		'rust',
+		'tsx',
+		'typescript',
+		'vimdoc',
+		'vim',
+		'dart',
+		'dockerfile',
+		'yaml',
+		'toml',
+	},
 	auto_install = false,
 	highlight = { enable = true },
 	indent = { enable = true },
