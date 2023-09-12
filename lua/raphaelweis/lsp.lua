d9a2df4e26fac63ff0b89bbcdd4dff134fc6d581
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-09-12 17:27:42 +0200

    lsp config changes

--- lua/raphaelweis/lsp.lua
+++ lua/raphaelweis/lsp.lua
@@ -1,3 +1,107 @@
+local function cmpConfig()
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+	-- [[ Configure nvim-cmp ]]
+	local cmp = require 'cmp'
+	local luasnip = require 'luasnip'
+	require('luasnip.loaders.from_vscode').lazy_load()
+	luasnip.config.setup {
+	}
+
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				luasnip.lsp_expand(args.body)
+			end,
+		},
+		window = {
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
+		},
+		mapping = cmp.mapping.preset.insert {
+			['<Alt-j>'] = cmp.mapping.select_next_item(),
+			['<Alt-k>'] = cmp.mapping.select_prev_item(),
+			['<C-d>'] = cmp.mapping.scroll_docs(-4),
+			['<C-f>'] = cmp.mapping.scroll_docs(4),
+			['<C-Space>'] = cmp.mapping.complete {},
+			['<CR>'] = cmp.mapping.confirm {
+				select = true,
+			},
+			['<Tab>'] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expand_or_locally_jumpable() then
+					luasnip.expand_or_jump()
+				else
+					fallback()
+				end
+			end, { 'i', 's' }),
+			['<S-Tab>'] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.locally_jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, { 'i', 's' }),
+		},
+		sources = {
+			{ name = 'nvim_lsp' },
+			{ name = 'luasnip' },
+		},
+	})
+end
+
+local function lspConfig()
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	-- Language servers configuration
+	-- import lspconfig plugin
+	local lspconfig = require("lspconfig")
+	local on_attach = function(_, bufnr)
+		local opts = { buffer = bufnr }
+		vim.keymap.set("n", "gR", "<CMD>Telescope lsp_references<CR>", opts)
+		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gD", "<CMD>Telescope lsp_definitions<CR>", opts)
+		vim.keymap.set("n", "gi", "<CMD>Telescope lsp_implementations<CR>", opts)
+		vim.keymap.set("n", "gt", "<CMD>Telescope lsp_type_definitions<CR>", opts)
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "gpd", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "gnd", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	end
+
+	-- Configure language servers
+	lspconfig["lua_ls"].setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		settings = { -- custom settings for lua
+			Lua = {
+				-- make the language server recognize "vim" global
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					-- make language server aware of runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.stdpath("config") .. "/lua"] = true,
+					},
+				},
+			},
+		},
+	})
+
+	lspconfig["gopls"].setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+	})
+end
+
 return {
 	'neovim/nvim-lspconfig',
 	dependencies = {
@@ -8,135 +112,8 @@ return {
 		'rafamadriz/friendly-snippets',
 	},
 	config = function()
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-		-- [[ Configure nvim-cmp ]]
-		local cmp = require 'cmp'
-		local luasnip = require 'luasnip'
-		require('luasnip.loaders.from_vscode').lazy_load()
-		luasnip.config.setup {
-		}
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = cmp.mapping.preset.insert {
-				['<Alt-j>'] = cmp.mapping.select_next_item(),
-				['<Alt-k>'] = cmp.mapping.select_prev_item(),
-				['<C-d>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete {},
-				['<CR>'] = cmp.mapping.confirm {
-					select = true,
-				},
-				['<Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
-				['<S-Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
-			},
-			sources = {
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },
-			},
-		})
-
-		-- Language servers configuration
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
-		local opts = { noremap = true, silent = true }
-		local on_attach = function(client, bufnr)
-			opts.buffer = bufnr
-
-			-- set keybinds
-			opts.desc = "Show LSP references"
-			vim.keymap.set("n", "gR", "<CMD>Telescope lsp_references<CR>", opts)
-
-			opts.desc = "Format current file"
-			vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, opts)
-
-			opts.desc = "Go to declaration"
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-
-			opts.desc = "Show LSP definitions"
-			vim.keymap.set("n", "gD", "<CMD>Telescope lsp_definitions<CR>", opts)
-
-			opts.desc = "Show LSP implementations"
-			vim.keymap.set("n", "gi", "<CMD>Telescope lsp_implementations<CR>", opts)
-
-			opts.desc = "Show LSP type definitions"
-			vim.keymap.set("n", "gt", "<CMD>Telescope lsp_type_definitions<CR>", opts)
-
-			opts.desc = "See available code actions"
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-			opts.desc = "Smart rename"
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-
-			opts.desc = "Show buffer diagnostics"
-			vim.keymap.set("n", "<leader>D", "<CMD>Telescope diagnostics bufnr=0<CR>", opts)
-
-			opts.desc = "Show line diagnostics"
-			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-
-			opts.desc = "Go to previous diagnostic"
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-
-			opts.desc = "Go to next diagnostic"
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-
-			opts.desc = "Show documentation for what is under cursor"
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		end
-
-		-- Configure language servers
-		lspconfig["lua_ls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = { -- custom settings for lua
-				Lua = {
-					-- make the language server recognize "vim" global
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						-- make language server aware of runtime files
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-							[vim.fn.stdpath("config") .. "/lua"] = true,
-						},
-					},
-				},
-			},
-		})
-
-		lspconfig["gopls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-
-
+		cmpConfig()
+		lspConfig()
 		-- Configure format on save
 		-- Create an augroup that is used for managing our formatting autocmds.
 		--      We need one augroup per client to make sure that multiple clients
