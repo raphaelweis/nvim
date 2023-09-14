987c2ab380e9f6f2f9e4fef3a64a775bb25e4442
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-09-14 11:01:57 +0200

    fixed bug with treesitter configuration and flutter-tools configuration

--- lua/raphaelweis/treesitter.lua
+++ lua/raphaelweis/treesitter.lua
@@ -26,14 +26,17 @@ return {
 			},
 			auto_install = false,
 			highlight = { enable = true },
-			indent = { enable = true },
+			indent = {
+				enable = true,
+				-- The flutter-tools plugins and treesitter indent are in conflict
+				-- so we disable it here, and let the plugin handle it.
+				disable = { 'dart' },
+			},
 			incremental_selection = {
 				enable = true,
 				keymaps = {
 					init_selection = '<c-space>',
 					node_incremental = '<c-space>',
-					scope_incremental = '<c-s>',
-					node_decremental = '<M-space>',
 				},
 			},
 		}
