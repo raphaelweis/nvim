21ff46e364fc734ed076eb3ebc6539964a2aeb95
Raphaël Weis <raphael.weis.dev@gmail.com>
2023-12-27 19:45:46 +0100

    added the essential plugins

--- init.lua
+++ init.lua
@@ -1,6 +1,24 @@
-require("raphaelweis.options")
-require("raphaelweis.keymaps")
+-- Options and Keymaps
+vim.g.mapleader = " "
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.colorcolumn = "80"
+vim.opt.undofile = true
+vim.opt.clipboard:append("unnamedplus")
+
+vim.keymap.set("n", "<Esc>", "<CMD>noh<CR>")
+
+-- Lazy Bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -14,149 +32,99 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = function()
-			require("raphaelweis.gruvbox")
-		end,
+-- Lazy configuration
+local plugins = {
+	"tpope/vim-surround",
+	"tpope/vim-fugitive",
+	{ "ellisonleao/gruvbox.nvim", opts = { transparent_mode = true } },
+	{ "numToStr/Comment.nvim", opts = {}, lazy = false },
+	{ "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
+	{ "mhartington/formatter.nvim", opts = {} },
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	"neovim/nvim-lspconfig",
+}
+require("lazy").setup(plugins, {})
+
+-- Treesitter configuration
+require("nvim-treesitter.configs").setup({
+	auto_install = true,
+	highlight = { enable = true },
+})
+
+-- Formatter configuration
+require("formatter").setup({
+	filetype = {
+		c = { require("formatter.filetypes.c").clangformat },
+		lua = { require("formatter.filetypes.lua").stylua },
+		["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
 	},
-	{
-		"troydm/zoomwintab.vim",
-		config = function()
-			require("raphaelweis.zoomwintab")
+})
+
+-- LSP configuration
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<space>f", function()
+			vim.lsp.buf.format({ async = true })
+		end, opts)
+	end,
+})
+
+require("lspconfig").lua_ls.setup({
+	on_init = function(client)
+		local path = client.workspace_folders[1].name
+		if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+			client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+				Lua = {
+					runtime = { version = "LuaJIT" },
+					workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } },
+				},
+			})
+			client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+		end
+		return true
+	end,
+})
+
+-- Cmds
+vim.cmd("colorscheme gruvbox")
+
+-- Autocmds
+vim.api.nvim_create_augroup("__formatter__", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", { group = "__formatter__", command = ":FormatWrite" })
+
+-- helper function for indentation based on filetype
+local function setIndentation(filetype, indent_size)
+	vim.api.nvim_create_autocmd("FileType", {
+		callback = function()
+			vim.bo.shiftwidth = indent_size
+			vim.bo.tabstop = indent_size
 		end,
-	},
-	{ "nvim-lua/plenary.nvim" },
-	{ "tpope/vim-fugitive" },
-	{ "christoomey/vim-tmux-navigator" },
-	{
-		"nvim-tree/nvim-tree.lua",
-		config = function()
-			require("raphaelweis.tree")
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			require("raphaelweis.autopairs")
-		end,
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("raphaelweis.gitsigns")
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("raphaelweis.indent_blankline")
-		end,
-	},
-	{
-		"folke/neodev.nvim",
-		config = function()
-			require("raphaelweis.neodev")
-		end,
-	},
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("raphaelweis.comment")
-		end,
-	},
-	{
-		"ThePrimeagen/harpoon",
-		config = function()
-			require("raphaelweis.harpoon")
-		end,
-	},
-	{
-		"lervag/vimtex",
-		config = function()
-			require("raphaelweis.vimtex")
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = {
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-			},
-		},
-		config = function()
-			require("raphaelweis.telescope")
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		build = ":TSUpdate",
-		config = function()
-			require("raphaelweis.treesitter")
-		end,
-	},
-	{
-		"stevearc/dressing.nvim",
-		config = function()
-			require("raphaelweis.dressing")
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lsp",
-			"rafamadriz/friendly-snippets",
-		},
-		config = function()
-			require("raphaelweis.cmp")
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"jay-babu/mason-nvim-dap.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-		},
-		config = function()
-			require("raphaelweis.mason")
-		end,
-	},
-	{
-		"mhartington/formatter.nvim",
-		config = function()
-			require("raphaelweis.formatter")
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"akinsho/flutter-tools.nvim",
-		},
-		config = function()
-			require("raphaelweis.lsp")
-		end,
-	},
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			"leoluz/nvim-dap-go",
-		},
-		config = function()
-			require("raphaelweis.dap")
-		end,
-	},
-}, {})
+		pattern = filetype,
+	})
+end
+setIndentation("html", 2)
--- lua/raphaelweis/zoomwintab.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set("n", "<leader>sm", "<CMD>ZoomWinTabToggle<CR>") -- toggle maximize current window
