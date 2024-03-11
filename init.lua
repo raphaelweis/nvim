40866f749551a0750dd8b1cdb5fe3de69cf80add
Raphaël Weis <raphael.weis.dev@gmail.com>
2024-03-11 08:43:47 +0100

    feat: added regex highlights for markdown files.

--- init.lua
+++ init.lua
@@ -146,6 +146,7 @@ require("nvim-treesitter.configs").setup({
 	},
 	auto_install = true,
 	highlight = { enable = true, disable = { "latex" } },
+	additional_vim_regex_highlighting = { "markdown" },
 	autotag = { enable = true },
 })
 
